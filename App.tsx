92ddcee82b4fa357105f1345acfcfab1c9848bd5
AtifKhokhar <atif.khokhar@telefonica.com>
2021-02-16 17:00:08 +0000

    tweaking styling and adding in navigation to details page

--- App.tsx
+++ App.tsx
@@ -4,11 +4,18 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Card, ListItem, Button, Icon, Avatar } from "react-native-elements";
+import {
+  Card,
+  ListItem,
+  Button,
+  Icon,
+  Avatar,
+  Tile,
+} from "react-native-elements";
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, flexWrap: "wrap", flexDirection: "row", justifyContent: "center" }}>
       <Card>
         <Card.Title>House Number 1</Card.Title>
         <Card.Divider style={{ marginBottom: 10 }} />
@@ -31,8 +38,35 @@ function HomeScreen() {
             marginBottom: 0,
           }}
           title="VIEW NOW"
+          onPress={() => navigation.navigate("Details")}
         />
       </Card>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, flexWrap: "wrap", flexDirection: "row" }}>
+      <Tile
+        imageSrc={{
+          uri:
+            "https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041_1280.jpg",
+        }}
+        title="5 bed modern contemporary property with 3 bathrooms and garage. Freehold property near ameneties."
+        titleStyle={{  flex: 0, flexDirection: 'column', flexWrap: 'nowrap' }}
+      >
+        <View
+          style={{
+            flex: 0,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>Contact Agent at : 00000000000</Text>
+          <Text>Price: £900,000</Text>
+        </View>
+      </Tile>
     </View>
   );
 }
@@ -46,6 +80,20 @@ function App() {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
+          options={{
+            title: "Property Listings",
+            headerStyle: {
+              backgroundColor: "#707472",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
           options={{
             title: "Property Listings",
             headerStyle: {
